ba1597405e1e96094a7f4a94c7fbe82fa075f98f
felix1429 <thennig314@gmail.com>
2014-03-26 08:10:23 -0500

    formating for Weight.java

--- src/Weight.java
+++ src/Weight.java
@@ -2,15 +2,12 @@ public class Weight {
 	
 	Boolean isWeighted;
 	
-	
 	public Weight(Boolean startWeight) {
 		this.isWeighted = startWeight;
 	}
 	
+	
 	public Boolean getWeight() {
 		return isWeighted;
-	}
-	
-	
-		
+	}	
 }
