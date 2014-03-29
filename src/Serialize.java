977a5c6859fc5efc45fef340aa281b292a22e378
felix1429 <thennig314@gmail.com>
2014-03-29 02:40:07 -0500

    updated serialize.java, created main() that initializes paths

--- src/Serialize.java
+++ src/Serialize.java
@@ -1,29 +1,44 @@
-//ITFP: C:/users/216003/java/eclipse windows/workbenchProjects/gradebook/savedData/data.ser
+//ITFP: C:/users/216003/java/eclipse windows/workbenchProjects/gradebook/data/data.ser
+//HOME: C:/Users/Hennig/workspace/Gradebook/data/data.ser
 
 import java.io.*;
+import java.util.*;
 
 public class Serialize
 	implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	String path = "C:/users/216003/java/eclipsewindows/workbenchprojects/gradebook/data/data.ser";
+	String homePath = "C:/Users/Hennig/workspace/Gradebook/data/data.ser";
+	String ITFPPath = "C:/users/216003/java/eclipse windows/workbenchProjects/gradebook/data/data.ser";
+	List<String> pathList = new ArrayList<String>(); //list which will contain paths of various places on different computers I save crap in
 	Gradebook gradeBk;
+	String path;
+	
+	public void main(String[] args) {
+		pathList.add(homePath); 
+		pathList.add(ITFPPath);
+		
+		for(String i : pathList) { //tests all paths in list and if finds legit one, sets it to 'path' var
+			File file = new File(i);
+			if(file.exists()) {
+				path = i;
+				break;
+			}
+		}	
+	}
 	
 	public void serialize(Object ... theObj) {
 		try {
-			File file = new File(path);
-			if(!file.exists()) {
-				file.createNewFile();
-			}
-			FileOutputStream fOut = new FileOutputStream(file);
+			FileOutputStream fOut = new FileOutputStream(path);
 			ObjectOutputStream oOut = new ObjectOutputStream(fOut);
 			for(int i = 0;i < theObj.length; i ++) {
 				oOut.writeObject(theObj[i]);
 			}
 			oOut.close();
 			fOut.close();
-		}catch(IOException i) {
-			i.printStackTrace();
+		}catch(IOException io) {
+			io.printStackTrace();
+			System.out.println("Error, project not saved");
 		}
 	}
 	
@@ -37,7 +52,8 @@ public class Serialize
 		}catch(IOException i) {
 			i.printStackTrace();
 		}catch(ClassNotFoundException c) {
-			System.out.println("Error");
+			c.printStackTrace();
+			System.out.println("Error in decoding information");
 		}	
 	}
 }
