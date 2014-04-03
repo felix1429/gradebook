d50387b441ac68c2fafb47063ce12c389070fe76
felix1429 <thennig314@gmail.com>
2014-04-03 08:31:09 -0500

    edited getting of weight so missing excused/not excused is taken into account

--- src/MissingAssignment.java
+++ src/MissingAssignment.java
@@ -1,13 +1,26 @@
+import java.io.Serializable;
 
 public class MissingAssignment
-	extends Assignment {
+	extends Assignment 
+	implements Serializable {
 	
-	Boolean isExcused;
+	private static final long serialVersionUID = 1L;
+	Boolean isExcused; //if is an excused assignment or not  
+	 // if assignment is excused, will be put in as "missing" but no points will be docked
+	 // if not excused, put in as zero, counting towards total grade
 
 	public MissingAssignment(Score inputScore, Integer startNumber, String startName, //constructor on multiple lines
 			Boolean startSetWeight, Double startingWeight, Boolean isExcused) {		  // because of length
-		super(inputScore, startNumber, startName, startSetWeight, startingWeight);
+		super(inputScore, startNumber, startName, startSetWeight, startingWeight);  //super for Assignment() class
 		this.isExcused = isExcused;
+	}
+	
+	public Boolean getExcusedBool() {
+		return this.isExcused;
+	}
+	
+	public void setExcused(Boolean bool) {
+		this.isExcused = bool;
 	}
 
 }
